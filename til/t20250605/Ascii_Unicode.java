7ef59b7a5d8d6f768b6eaf67a8093c8b01553ad8
skymoon7337 <skymoon7337@gmail.com>
2025-06-05 16:05:30 +0900

    Edit

--- til/t20250605/Ascii_Unicode.java
+++ til/t20250605/Ascii_Unicode.java
@@ -1,10 +1,18 @@
 package til.t20250605;
 
-public class Ascii_Char {
+public class Ascii_Unicode {
     public static void main(String[] args) {
          char asciiChar = 'A';
          System.out.println("Character: "+ asciiChar);
+
          int asciicode= (int) asciiChar;
          System.out.println("Asciicode: "+ asciicode);
+
+        char kC = '가';
+        System.out.println("Character: "+ kC);
+
+        int unicode= (int) kC;
+        System.out.println("Asciicode: "+ Integer.toHexString(unicode).toUpperCase());
+
     }
 }
