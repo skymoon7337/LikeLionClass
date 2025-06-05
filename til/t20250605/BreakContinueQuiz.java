96fbe8f2a2027f11c843cd6b8ee4f40c41a989c3
skymoon7337 <skymoon7337@gmail.com>
2025-06-05 17:07:10 +0900

    Edit

---
+++ til/t20250605/BreakContinueQuiz.java
@@ -0,0 +1,17 @@
+package til.t20250605;
+
+public class BreakContinueQuiz {
+    public static void main(String[] args) {
+         for (int i=1;i<=30;i++){
+
+             if (i==20){
+                 break;
+             }
+              if (i%6!=0){
+                  System.out.println(i);
+              }
+
+         }
+
+    }
+}
