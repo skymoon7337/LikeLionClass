4cf556f66a487f369fcd58226e783f0c8acdc111
skymoon7337 <skymoon7337@gmail.com>
2025-06-09 15:47:22 +0900

    Edit

---
+++ til/t20250609/StringGreeting.java
@@ -0,0 +1,11 @@
+package til.t20250609;
+
+public class StringGreeting {
+    public static void main(String[] args) {
+
+         String greeting = "Hello, Alice!";
+
+         System.out.println(greeting.toLowerCase());
+         System.out.println(greeting.length());
+    }
+}
