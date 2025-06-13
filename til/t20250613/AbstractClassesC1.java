c4368ab2275190fc6c9ff23784ea3c18ec4cef87
skymoon7337 <skymoon7337@gmail.com>
2025-06-13 14:50:22 +0900

    Edit

---
+++ til/t20250613/AbstractClassesC1.java
@@ -0,0 +1,25 @@
+package til.t20250613;
+
+abstract class AnimalC3{
+     abstract void makeSound();
+
+     public void sleep(){
+          System.out.println("잔다");
+     }
+}
+
+class DogC3 extends AnimalC3 {
+     @Override
+     void makeSound() {
+          System.out.println("멍");
+     }
+}
+
+public class AbstractClassesC1 {
+    public static void main(String[] args) {
+         AnimalC3 dog = new DogC3();
+
+         dog.makeSound();
+         dog.sleep();
+    }
+}
