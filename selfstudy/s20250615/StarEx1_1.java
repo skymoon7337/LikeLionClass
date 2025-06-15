95675cf6bb4ae6a589218d231ca7b18b6ec4f064
skymoon7337 <skymoon7337@gmail.com>
2025-06-15 22:20:30 +0900

    Edit

---
+++ selfstudy/s20250615/StarEx1_1.java
@@ -0,0 +1,43 @@
+package selfstudy.s20250615;
+
+/*
+1         4         좌공백 3+ 우공백3 + 별 1
+2        345        좌공백 2+ 우공백2 + 별 3
+3       23456       좌공백 1+ 우공백1 + 별 5
+4      1234567      좌공백 0+ 우공백0 + 별 7
+5        345        좌공백 2+ 우공백2 + 별 3
+6        345        좌공백 2+ 우공백2 + 별 3
+7        345        좌공백 2+ 우공백2 + 별 3
+ */
+
+
+public class StarEx1_1  {
+     public static void main(String[] args) {
+          for (int i = 1; i < 8; i++) {
+               int space;
+               int count;
+
+               if (i <= 4) {
+                    space = 3 - (i-1);
+                    count = 2 * (i-1) + 1;
+               } else {
+                    space = 2;
+                    count = 3;
+               }
+
+               for (int s = 0; s < space; s++) {
+                    System.out.print(" ");
+               }
+
+               for (int n = 0; n < count; n++) {
+                    System.out.print("*");
+               }
+
+               for (int s = 0; s < space; s++) {
+                    System.out.print(" ");
+               }
+
+               System.out.println();
+          }
+     }
+}
