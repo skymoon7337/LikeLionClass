49e2f1b790b4d19d12bf8d36e4400aaf5861aef3
skymoon7337 <skymoon7337@gmail.com>
2025-06-16 11:07:04 +0900

    Edit

--- til/t20250616/InnerC2.java
+++ til/t20250616/InnerC2.java
@@ -1,32 +1,28 @@
 package til.t20250616;
 
-class CarC2{
+class CarC2 {
      String brand;
+     Engine engine;
 
-     public CarC2(String brand){
-          this.brand=brand;
+     public CarC2(String brand) {
+          this.brand = brand;
+          this.engine = new Engine();
      }
 
-     class Engine{
-          public void start(){
-               System.out.println("The engine of "+brand+"is starting.");
-
+     class Engine {
+          public void start() {
+               System.out.println("The engine of " + brand + " is starting.");
           }
      }
 
-     public void startEngine(){
-          Engine engine = new Engine();
+     public void startEngine() {
           engine.start();
-
      }
 }
 
-
-
 public class InnerC2 {
-    public static void main(String[] args) {
-         CarC2 car=new CarC2("아우디");
-         car.startEngine();
-
-    }
+     public static void main(String[] args) {
+          CarC2 car = new CarC2("아우디");
+          car.startEngine();
+     }
 }
