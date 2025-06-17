43c749681f8148c7ffa5b342f1d02c20e52e7564
skymoon7337 <skymoon7337@gmail.com>
2025-06-17 14:32:57 +0900

    Edit

---
+++ til/t20250617/ThrowsCL2.java
@@ -0,0 +1,26 @@
+package til.t20250617;
+
+public class ThrowsCL2 {
+          public static void main(String[] args) {
+               String input = "100";
+
+               try {
+                    boolean result = validateTokenCount(input);
+
+                    if (result) {
+                         System.out.println("숫자 2개");
+                    }
+               } catch(NumberFormatException e) {
+                    System.out.println(e.getMessage());
+               }
+
+          }
+
+          public static boolean validateTokenCount(String tokens) throws NumberFormatException {
+               if (tokens.length() != 2) {
+                    throw new NumberFormatException("숫자 2개가 아닙니다.");
+               }
+
+               return true;
+          }
+     }
