7d22d5de055d5f7f501b72d2a6961667cb33002a
skymoon7337 <skymoon7337@gmail.com>
2025-06-22 16:13:48 +0900

    Edit

--- makes/m3_daycalculator/DayCalculator_V1.java
+++ makes/m3_daycalculator/DayCalculator_V1.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class DayCalculator_V1 {
     public static void main(String[] args) {
 
+    // 무한 루프때 계속 생성됨으로, while문 밖으로 뺌
     Scanner scanner = new Scanner(System.in);
 
+    //반복 실행 되게 while 사용
     while (true) {
 
-        // 입력부
 
         int inputyear;
         int inputmonth;
@@ -37,7 +38,6 @@ public class DayCalculator_V1 {
         }
 
         // Zeller 보정으로 인해 변경될 수 있는 요일들의 원본 저장
-
         int year = inputyear;
         int month = inputmonth;
         int day = inputday;
@@ -46,14 +46,12 @@ public class DayCalculator_V1 {
         // Zeller 공식의 1월,2월 보정
         // 2월은 윤년 등등 때문에 예외발생
         // 3~12월의 반복으로 패턴화, 즉 당해 1월을 전년도 13월, 2월을 14월 이라고 침
-
         if (inputmonth == 1 || inputmonth == 2) {
             inputmonth += 12;
             inputyear--;
         }
 
         // Zeller 공식 계산식
-
         int k;
         int j;
         int resultday;
@@ -65,7 +63,6 @@ public class DayCalculator_V1 {
         ;
 
         // resultday -> 0(토요일), 1(일요일), 2(월요일), 3(화요일), 4(수요일), 5(목요일), 6(금요일)
-
         String[] days = {"토", "일", "월", "화", "수", "목", "금"};
 
         System.out.printf("%d년 %d월 %d일은 %s요일 입니다.\n", year, month, day, days[resultday]);
@@ -84,5 +81,5 @@ public class DayCalculator_V1 {
     }
 }
 
-//2,4,6,9,11월의 날짜 입력 제한 필요
+//2,4,6,9,11월의 날짜 입력 제한 필요(ex: 4월 31일 불가)
 //캘린더와 연결해 해당 날짜 띄워서 맞는지 확인 해보면 좋을듯
