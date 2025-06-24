a70c10d86b56b91ae98a42129be01c07df6570b7
skymoon7337 <skymoon7337@gmail.com>
2025-06-24 13:09:11 +0900

    Edit

--- til/y2025/m06/d24/WriteFile1.java
+++ til/y2025/m06/d24/WriteFile1.java
@@ -1,30 +1,36 @@
 package til.y2025.m06.d24;
 
-import java.io.FileWriter;
-import java.io.IOException;
-
+import java.io.*;
 
 public class WriteFile1 {
     public static void writeFile(String filename,String content) {
-        FileWriter writer = null;
-
-        try{
-            writer=new FileWriter(filename);
+        try (FileWriter writer = new FileWriter(filename,true)) {
             writer.write(content);
-        } catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        } finally{
-            try{
-                if (writer !=null) writer.close();
-            } catch(IOException e){
-                e.printStackTrace();
+        }
+    }
+
+    public static void readFile(String filename) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
 
     public static void main(String[] args) {
-        writeFile("lunch-menu.txt","오늘 점심은?");
-        System.out.println("파일 생성 완료");
+
+    // 파일 생성
+    //    writeFile("lunch-menu.txt","\n스테이크");
+    //    System.out.println("파일 생성 완료");
+
+    //파일 읽기
+        readFile("lunch-menu.txt");
     }
 }
