b6ec12c3b8e08f0b45c47e573471b93bf098bde5
skymoon7337 <skymoon7337@gmail.com>
2025-06-25 15:28:57 +0900

    First

--- src/main/java/com/skymoon7337/hello/controller/HelloController.java
+++ src/main/java/com/skymoon7337/hello/controller/HelloController.java
@@ -6,13 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
-    }
 
     @GetMapping("/")
-    public String home() {
+    public String hello() {
         return "home";
     }
 }
