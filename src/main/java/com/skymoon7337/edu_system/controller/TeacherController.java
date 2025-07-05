985681a3d1868cc67f83455bc00f254ddead45fd
skymoon7337 <skymoon7337@gmail.com>
2025-07-05 22:36:38 +0900

    edit

--- src/main/java/com/skymoon7337/edu_system/controller/TeacherController.java
+++ src/main/java/com/skymoon7337/edu_system/controller/TeacherController.java
@@ -5,10 +5,7 @@ import com.skymoon7337.edu_system.repository.TeacherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/teachers")
@@ -32,6 +29,20 @@ public class TeacherController {
     @PostMapping("/add")
     public String add(@ModelAttribute Teacher teacher) {
         teacherRepository.save(teacher);
+
+        return "redirect:/teachers";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editForm(@PathVariable int id, Model model) {
+        model.addAttribute("teacher", teacherRepository.findById(id));
+
+        return "teacher-form";
+    }
+
+    @PostMapping("/edit")
+    public String edit(@ModelAttribute Teacher teacher) {
+        teacherRepository.update(teacher);
 
         return "redirect:/teachers";
     }
