28af6eaa74a389b4cabc089d5010b9f4607604c4
skymoon7337 <skymoon7337@gmail.com>
2025-07-08 11:08:47 +0900

    first

---
+++ src/main/java/com/skymoon7337/to_do_list_with_dto/dto/SignupDTO.java
@@ -0,0 +1,19 @@
+package com.skymoon7337.to_do_list_with_dto.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupDTO {
+    @NotBlank(message = "아이디를 입력하세요")
+    @Size(min=3, max=50, message = "아이디는 3~50자여야 합니다")
+    private String username;
+
+    @NotBlank(message = "비밀번호를 입력하세요")
+    @Size(min=6, max=20, message = "패스워드는 6~20자여야 합니다")
+    private String password;
+}
