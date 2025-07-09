f89ecb9bb223b3a52c4b3c9e357084fb9ad2c2f9
skymoon7337 <skymoon7337@gmail.com>
2025-07-09 14:55:15 +0900

    edit

---
+++ src/main/java/com/skymoon7337/simple_board/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.skymoon7337.simple_board.controller;
+
+import com.skymoon7337.simple_board.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+    private final UserRepository userRepository;
+
+    @GetMapping({"/", "/login"})
+    public String loginForm(Model model) {
+        model.addAttribute("loginDto", new LoginDto());
+
+        return "login";
+    }
+}
