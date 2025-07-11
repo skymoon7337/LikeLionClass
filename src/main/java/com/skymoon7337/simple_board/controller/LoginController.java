62b46a27c878c93e0c2c3cc2d98c70fc313f8c2b
skymoon7337 <skymoon7337@gmail.com>
2025-07-11 17:03:10 +0900

    edit

--- src/main/java/com/skymoon7337/simple_board/controller/LoginController.java
+++ src/main/java/com/skymoon7337/simple_board/controller/LoginController.java
@@ -51,6 +51,6 @@ public class LoginController {
     public String logout(HttpSession session) {
         session.invalidate();
 
-        return "redirect:/login";
+        return "redirect:/posts";
     }
 }
