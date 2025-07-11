eafeeeb02759781945bf7a16caf1ad1b310a1551
skymoon7337 <skymoon7337@gmail.com>
2025-07-11 15:22:46 +0900

    edit

---
+++ src/main/java/com/skymoon7337/book_management/model/Author.java
@@ -0,0 +1,13 @@
+package com.skymoon7337.book_management.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+    private Integer id;
+    private String name;
+}
