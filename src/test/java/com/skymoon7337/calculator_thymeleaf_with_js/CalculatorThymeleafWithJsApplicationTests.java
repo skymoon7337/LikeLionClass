6181c94ef593a402d5c986fca3f7971c68bceee8
skymoon7337 <skymoon7337@gmail.com>
2025-07-18 13:44:34 +0900

    edit

---
+++ src/test/java/com/skymoon7337/calculator_thymeleaf_with_js/CalculatorThymeleafWithJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skymoon7337.calculator_thymeleaf_with_js;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorThymeleafWithJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
