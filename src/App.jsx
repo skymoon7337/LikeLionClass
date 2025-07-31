b95f28aee8953bdca0e6c330070ca8ed3efe1732
skymoon7337 <skymoon7337@gmail.com>
2025-07-31 11:43:51 +0900

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
+
+// const App = () => {
+//   return (
+//     <div className="bg-gray-200 min-h-screen flex justify-center items-center">
+//       <Counter />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import InputText from "./components/InputText";
+
+// const App = () => {
+//   return (
+//     <div className="bg-red-100 min-h-screen flex justify-center items-center">
+//       <InputText />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import ItemAdd from "./components/ItemAdd";
 
 const App = () => {
   return (
-    <div className="bg-gray-200 min-h-screen flex justify-center items-center">
-      <Counter />
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      <ItemAdd />
     </div>
   );
 };
