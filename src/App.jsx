9cab120a93f1f352df72c9230b6a672238dac368
skymoon7337 <skymoon7337@gmail.com>
2025-08-01 11:02:55 +0900

    First

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ReactionTimerGame from "./components/ReactionTimerGame";
+
+const App = () => {
+  return (
+    <div className="bg-green-100 min-h-screen flex justify-center items-center">
+      <ReactionTimerGame />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ReactionTimerGame.jsx
@@ -0,0 +1,9 @@
+const ReactionTimerGame = () => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-8 p-8 bg-white shadow-xl">
+      <h1 className="text-2xl font-bold">⏱️ 10초 반응 게임</h1>
+    </div>
+  );
+};
+
+export default ReactionTimerGame;
